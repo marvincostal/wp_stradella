2168025c11271b3e5fbedcab333c7d5d05855c31
Marvin Costal <philwebservices.costal@gmail.com>
2015-03-18 17:47:47 +0800

    fixed contact us

--- page-contact.php
+++ page-contact.php
@@ -13,39 +13,7 @@ get_header(); ?>
       <h1 class="page-title text-center"><span>C</span>ontact Us</h1>
       <div class="row">
         <div class="col-md-6">
-          <form class="form-contact">
-            <div class="form-group">
-              <label for="firstname">First Name</label>
-              <input type="text" class="form-control" id="firstname">
-            </div>
-            <div class="form-group">
-              <label for="lastname">Last Name</label>
-              <input type="text" class="form-control" id="lastname">
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="contact">Contact Number</label>
-                  <input type="text" class="form-control" id="contact">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <label for="country">Country</label>
-                <select id="country" class="form-control">
-                  <option>Australia</option>
-                  <option>America</option>
-                  <option>Canada</option>
-                  <option>Japan</option>
-                  <option>Philippines</option>
-                </select>
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="message">Message</label>
-              <textarea id="message" class="form-control" rows="3"></textarea>
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-          </form>
+          <?php echo do_shortcode( '[contact-form-7 id="119" title="Contact Form"]' ); ?>
         </div>
         <div class="col-md-6">
           <ul class="contact-details">
